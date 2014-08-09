cb3763495e7871724f32f7b493e7e59e261a5a51
Rui Quelhas <rfpquelhas@gmail.com>
2014-08-09 13:23:51 +0100

    Update tests and make them run fast.

--- test/index.js
+++ test/index.js
@@ -2,7 +2,11 @@ var fs = require('fs');
 
 var Lab = require('lab');
 var Hapi = require('hapi');
+var Nipple = require('nipple');
 var FormData = require('form-data');
+
+var internals = {};
+internals.handler = function (request, reply) { return reply(); };
 
 var plugin = require('../lib/index');
 
@@ -10,42 +14,114 @@ Lab.experiment('multipart/form-data file upload', function () {
   var suite = {};
 
   Lab.before(function (done) {
+    suite.request = { method: 'POST' };
     suite.server = Hapi.createServer();
     suite.server.pack.register({
       plugin: plugin,
       options: {
         supported: ['jpg', 'png']
       }
-    }, done);
+    }, function (err) {
+      suite.server.route({
+        path: '/data',
+        method: 'POST',
+        config: {
+          payload: { output: 'data' },
+          handler: internals.handler
+        }
+      });
+
+      suite.server.route({
+        path: '/stream',
+        method: 'POST',
+        config: {
+          payload: { output: 'stream' },
+          handler: internals.handler
+        }
+      });
+
+      suite.server.route({
+        path: '/file',
+        method: 'POST',
+        config: {
+          payload: { output: 'file' },
+          handler: internals.handler
+        }
+      });
+
+      done();
+    });
   });
 
-  Lab.experiment('the files are parsed as data buffers', function () {
-    Lab.test('returns an OK response if the file type is valid', function (done) {
-      done();
+  Lab.experiment('when the file type is supported, the file should be upladed', function () {
+    Lab.beforeEach(function (done) {
+      suite.form = new FormData();
+      suite.form.append('file', fs.createReadStream('resources/file.png'));
+      Nipple.read(suite.form, function (err, buffer) {
+        suite.request.payload = buffer;
+        suite.request.headers = suite.form.getHeaders();
+        done();
+      });
     });
 
-    Lab.test('returns a unsupported media type error if the file type is invalid', function (done) {
-      done();
+    Lab.test('after it is read fully into memory', function (done) {
+      suite.request.url = '/data';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    Lab.test('if it is streamed by the server', function (done) {
+      suite.request.url = '/stream';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    Lab.test('if it is written to a temporary reference', function (done) {
+      suite.request.url = '/file';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(200);
+        done();
+      });
     });
   });
 
-  Lab.experiment('the files are parsed as streams', function () {
-    Lab.test('returns an OK response if the file type is valid', function (done) {
-      done();
+  Lab.experiment('when the file type is not supported, the server should return an error', function () {
+    Lab.beforeEach(function (done) {
+      suite.form = new FormData();
+      suite.form.append('file', fs.createReadStream('resources/file.invalid'));
+      Nipple.read(suite.form, function (err, buffer) {
+        suite.request.payload = buffer;
+        suite.request.headers = suite.form.getHeaders();
+        done();
+      });
     });
 
-    Lab.test('returns a unsupported media type error if the file type is invalid', function (done) {
-      done();
-    });
-  });
-
-  Lab.experiment('the files are parsed as temporary files', function () {
-    Lab.test('returns an OK response if the file type is valid', function (done) {
-      done();
+    Lab.test('after it is read fully into memory', function (done) {
+      suite.request.url = '/data';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(415);
+        done();
+      });
     });
 
-    Lab.test('returns a unsupported media type error if the file type is invalid', function (done) {
-      done();
+    Lab.test('if it is streamed by the server', function (done) {
+      suite.request.url = '/stream';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(415);
+        done();
+      });
+    });
+
+    Lab.test('if it is written to a temporary reference', function (done) {
+      suite.request.url = '/file';
+      suite.server.inject(suite.request, function (response) {
+        Lab.expect(response.statusCode).to.equal(415);
+        done();
+      });
     });
   });
 });
