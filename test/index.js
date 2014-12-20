66e7e2e8e5e2de19687072542ca44050d91d5437
Rui Quelhas <rui@alpha-iv.com>
2014-12-20 17:13:49 +0000

    Update implementation supporting payload parsing as Buffer.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs');
 
 var _ = require('lodash');
@@ -12,6 +14,12 @@ var lab = exports.lab = Lab.script();
 var plugin = require('../');
 
 var internals = {};
+
+internals.defaults = {
+    temporary: {
+        file: 'test/tmp/file'
+    }
+};
 
 internals.boostrap = function (config /*, options, fn */) {
 
@@ -67,14 +75,37 @@ lab.describe('Data parsing-enabled file upload', function () {
 
     lab.before(function (done) {
 
-        var config = {
-            payload: {
-                output: 'data',
-                parse: true
-            },
-            handler: function (request, reply) {
+        payload = {
+            output: 'data',
+            parse: true
+        };
+
+        var handler = function (request, reply) {
+            var buffer = request.payload.file;
+            var tmp = internals.defaults.temporary.file;
+
+            var done = function (err) {
+                if (err) {
+                    return reply(err);
+                }
+
                 return reply();
-            }
+            };
+
+            var save = function (err, fd) {
+                if (err) {
+                    return reply(err);
+                }
+
+                fs.write(fd, buffer, 0, buffer.length, 0, done);
+            };
+
+            fs.open(tmp, 'w+', save);
+        };
+
+        var config = {
+            payload: payload,
+            handler: handler
         };
 
         internals.boostrap(config, function (err, instance) {
@@ -88,30 +119,72 @@ lab.describe('Data parsing-enabled file upload', function () {
         });
     });
 
-    lab.test('Returns OK if the media type is supported.', function (done) {
+    lab.afterEach(function (done) {
 
-        var file = 'test/static/file.png';
-        internals.append(file, function (request) {
-
-            server.inject(request, function (response) {
-
-                Code.expect(response.statusCode).to.equal(200);
-                return done();
-            });
+        var file = internals.defaults.temporary.file;
+        fs.unlink(file, function () {
+            return done();
         });
     });
 
-    lab.test('Returns error if the media type is not supported', function (done) {
+    lab.test('Returns OK if media type is supported.', function (done) {
+
+        var file = 'test/static/file.png';
+        var tmp = internals.defaults.temporary.file;
+
+        var size = function (file, fn) {
+
+            fs.stat(file, function (err, stats) {
+                if (err) {
+                    return done(err);
+                }
+
+                return fn(null, stats.size);
+            });
+        }
+
+        var verify = function (response) {
+
+            Code.expect(response.statusCode).to.equal(200);
+
+            size(file, function (original) {
+
+                size(tmp, function (copy) {
+
+                    Code.expect(original).to.equal(copy);
+                    return done();
+                });
+            });
+        };
+
+        var inject = function (request) {
+            server.inject(request, verify);
+        };
+
+        internals.append(file, inject);
+    });
+
+    lab.test('Returns error if media type is not supported', function (done) {
 
         var file = 'test/static/file.gif';
-        internals.append(file, function (request) {
+        var tmp = internals.defaults.temporary.file;
 
-            server.inject(request, function (response) {
+        var verify = function (response) {
 
-                Code.expect(response.statusCode).to.equal(415);
+            Code.expect(response.statusCode).to.equal(415);
+
+            fs.stat(tmp, function (err, copy) {
+                Code.expect(err).to.not.be.null();
+
                 return done();
             });
-        });
+        };
+
+        var inject = function (request) {
+            server.inject(request, verify);
+        };
+
+        internals.append(file, inject);
     });
 
 });
