e50faa7277f30fb3071b227c5c6303ee125645ce
Rui Quelhas <rfpquelhas@gmail.com>
2015-01-04 22:16:25 +0000

    Add support for raw stream validation (WIP - failing test).

--- test/stream/raw.js
+++ test/stream/raw.js
@@ -1,97 +1,77 @@
-// 'use strict';
+'use strict';
 
-// var fs = require('fs');
+var fs = require('fs');
+var Transform = require('stream').Transform;
 
-// var Lab = require('lab');
-// var Wreck = require('wreck');
-// var Subtext = require('subtext');
+var Lab = require('lab');
+var Wreck = require('wreck');
+var Subtext = require('subtext');
 
-// var lab = exports.lab = Lab.script();
+var lab = exports.lab = Lab.script();
 
-// var common = require('../common');
+var common = require('../common');
 
-// lab.describe('Stream raw upload', function () {
+lab.describe('Stream raw upload', function () {
 
-//     var server, payload;
+    var server, payload;
 
-//     lab.before(function (done) {
+    lab.before(function (done) {
 
-//         payload = {
-//             output: 'file',
-//             parse: false
-//         };
+        payload = {
+            output: 'stream',
+            parse: false
+        };
 
-//         var handler = function (request, reply) {
+        var handler = function (request, reply) {
 
-//             var tmp = 'test/tmp/file';
+            var options = {
+                output: 'data',
+                parse: true
+            };
 
-//             var options = {
-//                 output: 'data',
-//                 parse: true
-//             };
+            var copy = function (err, parsed) {
+                console.log('err:', err);
+                console.log('parsed:', parsed);
+                return reply();
+            };
 
-//             var done = function (err) {
+            console.log('request.payload:', request.payload);
 
-//                 if (err) {
-//                     return reply(err);
-//                 }
+            var stream = request.payload;
+            stream.headers = request.headers;
 
-//                 return reply();
-//             };
+            Subtext.parse(stream, null, options, copy);
+        };
 
-//             var copy = function (err, parsed) {
+        var config = {
+            payload: payload,
+            handler: handler
+        };
 
-//                 if (err) {
-//                     return reply(err);
-//                 }
+        common.boostrap(config, function (err, instance) {
 
-//                 fs.writeFile(tmp, parsed.payload.file, done);
-//             };
+            if (err) {
+                return done(err);
+            }
 
-//             var read = function (err, data) {
+            server = instance;
+            return done();
+        });
+    });
 
-//                 if (err) {
-//                     return reply();
-//                 }
+    lab.afterEach(function (done) {
 
-//                 var stream = Wreck.toReadableStream(data);
-//                 stream.headers = request.headers;
+        fs.unlink('test/tmp/file', function () {
+            return done();
+        });
+    });
 
-//                 Subtext.parse(stream, null, options, copy);
-//             };
+    lab.test('Returns OK if media type is supported.', function (done) {
+        common.verifyPositive(server, 'test/static/file.png', done);
+    });
 
-//             fs.readFile(request.payload.path, read);
-//         };
+    lab.test('Returns error if media type is not supported', function (done) {
+        common.verifyNegative(server, 'test/static/file.gif', done);
+    });
 
-//         var config = {
-//             payload: payload,
-//             handler: handler
-//         };
-
-//         common.boostrap(config, function (err, instance) {
-
-//             if (err) {
-//                 return done(err);
-//             }
-
-//             server = instance;
-//             return done();
-//         });
-//     });
-
-//     lab.afterEach(function (done) {
-
-//         fs.unlink('test/tmp/file', function () {
-//             return done();
-//         });
-//     });
-
-//     lab.test('Returns OK if media type is supported.', function (done) {
-//         common.verifyPositive(server, 'test/static/file.png', done);
-//     });
-
-//     lab.test('Returns error if media type is not supported', function (done) {
-//         common.verifyNegative(server, 'test/static/file.gif', done);
-//     });
-
-// });
+});
